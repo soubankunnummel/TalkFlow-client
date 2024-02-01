29576665be0afcc0ad2c109f2a861d99bab979b4
soubankunnummel <muhammedsoubankunnummel123@gmail.com>
2024-02-01 11:39:07 +0530

    fix:axios

--- app/service/axios.js
+++ app/service/axios.js
@@ -1,18 +1,26 @@
-// axiosInstance.js
+// // axiosInstance.js
 import axios from "axios";
 
-const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+// const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+// const Axios = axios.create({
+//   baseURL: baseUrl, 
+//   withCredentials:true,
+//   // headers: {
+//   //   'Content-Type': 'application/json',
+//   //   ...(typeof window !== 'undefined' && {
+//   //     Authorization: localStorage.getItem('jwt'),
+//   //   }),
+//   // },
+// });
+
+
+// export default Axios;
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL.replace(/\/$/, ''); 
 
 const Axios = axios.create({
-  baseURL: baseUrl, 
-  withCredentials:true,
-  // headers: {
-  //   'Content-Type': 'application/json',
-  //   ...(typeof window !== 'undefined' && {
-  //     Authorization: localStorage.getItem('jwt'),
-  //   }),
-  // },
+  baseURL: baseUrl,
+  withCredentials: true,
 });
-
 
 export default Axios;
